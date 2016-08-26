c46ed8161bbe2cae3d58e50da4e406c1173aff02
roman-zm <roman.zm98@gmail.com>
2016-08-26 20:45:59 +0300

    kek

--- main.cpp
+++ main.cpp
@@ -1,47 +1,65 @@
 #include <ncurses.h>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
-const int max = 12;
+const int max = 18;
 
 char table[max][max];
 
 const int cpos[4][2] = {
-    {0,5},
-    {0,6},
-    {1,5},
-    {1,6}
+    {0,max/2},
+    {1,max/2-1},
+    {1,max/2},
+    {1,max/2+1}
 };
 
-int pos[4][2] = {
-    {0,5},
-    {0,6},
-    {1,5},
-    {1,6}
+const int opos[4][2] = {
+    {0,max/2-1},
+    {0,max/2},
+    {1,max/2-1},
+    {1,max/2}
 };
 
-int prev_pos[4][2] = {
-    {0,7},
-    {0,8},
-    {1,7},
-    {1,8}
+const int upos[4][2] = {
+    {0,max/2},
+    {1,max/2},
+    {2,max/2},
+    {3,max/2}
 };
 
+int pos[4][2], prev_pos[4][2];
+std::vector<int> right_blocks, left_blocks, down_blocks;
 time_t secnds = time(NULL);
 
 
 void add_figure(){
+    int choice = rand()%3;
 
-    for(int i =0; i<4; i++)
-        for(int j = 0; j<2; j++){
-            pos[i][j]=cpos[i][j];
-            prev_pos[i][j]=cpos[i][j];
-        }
+    switch (choice) {
+    case 0:
+        for(int i =0; i<4; i++)
+            for(int j = 0; j<2; j++){
+                pos[i][j]=cpos[i][j];
+                prev_pos[i][j]=cpos[i][j];
+            }
+        break;
+    case 1:
+        for(int i =0; i<4; i++)
+            for(int j = 0; j<2; j++){
+                pos[i][j]=opos[i][j];
+                prev_pos[i][j]=opos[i][j];
+            }
+        break;
+    case 2:
+        for(int i =0; i<4; i++)
+            for(int j = 0; j<2; j++){
+                pos[i][j]=upos[i][j];
+                prev_pos[i][j]=upos[i][j];
+            }
+    }
 
 
-    //for(int i = 0; i<4; i++){       //Добавление фигуры
-    //    table[pos[i][0]][pos[i][1]] = '#';
-    //}
 }
 
 void write_figure(){
@@ -96,10 +114,29 @@ void print_table(){
     refresh();
 }
 
+bool is_free_right(){
+    bool free = true;
+    for(size_t i=0; i<right_blocks.size(); i++){
+        if(table[pos[right_blocks[i]][0]][pos[right_blocks[i]][1]+1] != ' ') free = false;
+        if(pos[right_blocks[i]][1] >= max-1) free = false;
+    }
+    return free;
+}
+
+bool is_free_left(){
+    bool free = true;
+    for(size_t i=0; i<left_blocks.size(); i++){
+        if(table[pos[left_blocks[i]][0]][pos[left_blocks[i]][1]-1] != ' ') free = false;
+        if(pos[left_blocks[i]][1] <= 0) free = false;
+    }
+    return free;
+}
+
+
 void move_right(){
-    //if (pos[0][1] < max-1 && pos[1][1] < max-1 && pos[2][1] < max-1 && pos[3][1] < max - 1 && ){
-    if(table[pos[1][0]][pos[1][1]+1] == ' ' && table[pos[3][0]][pos[3][1]+1] == ' ' &&
-            pos[1][1] < max-1 && pos[3][1] < max - 1){
+    //if(table[pos[1][0]][pos[1][1]+1] == ' ' && table[pos[3][0]][pos[3][1]+1] == ' ' && pos[1][1] < max-1 && pos[3][1] < max - 1)
+    if(is_free_right())
+    {
         for(int i =0; i<4; i++){
             for(int j =0; j<2; j++){
                 prev_pos[i][j] = pos[i][j];
@@ -118,8 +155,8 @@ void move_right(){
 }
 
 void move_left(){
-    if (pos[0][1] > 0 && pos[2][1] > 0 &&
-            table[pos[0][0]][pos[0][1]-1] == ' ' && table[pos[2][0]][pos[2][1]-1] == ' ' ){
+    if(is_free_left())
+    {
         for(int i =0; i<4; i++){
             for(int j =0; j<2; j++){
                 prev_pos[i][j] = pos[i][j];
@@ -137,14 +174,24 @@ void move_left(){
     }
 }
 
+bool is_free_down(){
+    bool free = true;
+    for(size_t i=0; i<down_blocks.size(); i++){
+        if(table[pos[down_blocks[i]][0]+1][pos[down_blocks[i]][1]] !=' ') free = false;
+    }
+    return free;
+}
+
 void fall_down(){
-    if(table[pos[2][0]+1][pos[2][1]] ==' ' && table[pos[3][0]+1][pos[3][1]]==' ' ){
+    //if(table[pos[2][0]+1][pos[2][1]] ==' ' && table[pos[3][0]+1][pos[3][1]]==' ' )
+    if(is_free_down()){
         for(int i =0; i<4; i++){
             for(int j =0; j<2; j++){
                 prev_pos[i][j] = pos[i][j];
             }
         }
-        while(table[pos[2][0]+1][pos[2][1]] ==' ' && table[pos[3][0]+1][pos[3][1]]==' ' ){
+        //while(table[pos[2][0]+1][pos[2][1]] ==' ' && table[pos[3][0]+1][pos[3][1]]==' ' )
+        while(is_free_down()){
             for(int i = 0; i<4; i++){
                 pos[i][0]+=1;
             }
@@ -163,8 +210,7 @@ void move_error(){
 void move_down(){
     if(secnds < time(NULL)){
         secnds = time(NULL);
-        if(table[pos[2][0]+1][pos[2][1]] ==' ' && table[pos[3][0]+1][pos[3][1]]==' ' &&
-                table[pos[1][0]+1][pos[1][1]] ==' ' && table[pos[0][0]+1][pos[0][1]] ==' '){
+        if(is_free_down()){
             for(int i =0; i<4; i++){
                 for(int j =0; j<2; j++){
                     prev_pos[i][j] = pos[i][j];
@@ -180,11 +226,77 @@ void move_down(){
     }
 }
 
+void analyze_figure(){
+    left_blocks.clear();
+    right_blocks.clear();
+    down_blocks.clear();
+
+    int lmin, rmin, dmin;
+    for(int i=0; i<4; i++){         //Левые и правые блоки
+        int lminint = max;
+        int rminint = 0;
+        for(int j=0; j<4; j++){
+            if(pos[i][0] == pos[j][0]){
+                if(pos[i][1] < pos[j][1] && pos[j][1] > rminint){
+                    rmin = j;
+                    rminint=pos[j][1];
+                }
+                if(pos[i][1] > pos[j][1] && pos[j][1] < lminint){
+                    lmin = j;
+                    lminint=pos[j][1];
+                }
+                if(pos[i][1] == pos[j][1]){
+                    if(pos[j][1] > rminint){
+                        rmin = j;
+                        rminint=pos[j][1];
+                    }
+                    if(pos[j][1] <lminint){
+                        lmin = j;
+                        lminint=pos[j][1];
+                    }
+                }
+            }
+        }
+        bool insrts = false;
+        for(size_t i=0; i<right_blocks.size(); i++)
+            if(right_blocks[i]==rmin) insrts = true;
+        if(!insrts) right_blocks.push_back(rmin);
+
+        insrts = false;
+        for(size_t i=0; i<left_blocks.size(); i++)
+            if(left_blocks[i]==lmin) insrts = true;
+        if(!insrts) left_blocks.push_back(lmin);
 
 
-int main()
-{
+    }
+
+    for(int i=0; i<4; i++){
+        int dminint = 0;
+        for(int j=0; j<4; j++){
+            if(pos[i][1] == pos[j][1]){
+                if(pos[i][0] < pos[j][0] && pos[j][0] > dminint){
+                    dmin = j;
+                    dminint=pos[j][0];
+                }
+                if(pos[i][0] == pos[j][0]){
+                    if(pos[j][0] > dminint){
+                        dmin = j;
+                        dminint=pos[j][0];
+                    }
+                }
+            }
+        }
+        bool insrts = false;
+        for(size_t i=0; i<down_blocks.size(); i++)
+            if(down_blocks[i]==dmin) insrts = true;
+        if(!insrts) down_blocks.push_back(dmin);
+    }
+}
+
+int main(){
     init_table();
+    add_figure();
+    analyze_figure();
     write_figure();
 
     initscr();
@@ -197,7 +309,6 @@ int main()
         clear();
         print_table();
 
-        //Получаем нажатие пользователя
         switch ( getch() )
         {
             case KEY_RIGHT:
@@ -217,6 +328,7 @@ int main()
         erase_figure();
         move_down();
         erase_line();
+        analyze_figure();
         write_figure();
 
     }//конец while(true)
